2152b8028d2a05348aa6615ad745b0c000b6eb42
Asuka-I <1400cm@gmail.com>
2021-06-04 07:30:21 +0900

    モデルとカラムを日本語化

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,22 +1,28 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.includes(:user).order(:created_at)
   end
 
   def new
+    @food = Food.new
   end
 
   def create
+    food = current_user.posts.create!(post_params)
+    redirect_to food
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def food_params
+    params.require(:post).permit(:content)
   end
 end
