2d44827fa6e75a38cf655510f96f6c41bbc9ce0d
Asuka-I <1400cm@gmail.com>
2021-06-13 23:58:43 +0900

    詳細表示・一覧表示に「いいね機能」を実装

--- db/migrate/20210611200838_create_likes.rb
+++ db/migrate/20210611200838_create_likes.rb
@@ -1,12 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :food_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
 
       t.timestamps
-
-      add_index :likes, %i[user_id food_id], unique: true
     end
+    add_index :likes, %i[user_id food_id], unique: true
   end
 end
