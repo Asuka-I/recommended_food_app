71b6ef2a2294300a50ae7e17b90515efb401739e
Asuka-I <1400cm@gmail.com>
2021-06-16 22:38:25 +0900

    task-8 修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -8,5 +8,13 @@ class Food < ApplicationRecord
     likes.any? { |like| like.user_id == user.id }
   end
 
+  def self.left_join_liked_foods(user_id)
+    join_sql = <<~SQL.squish
+      LEFT OUTER JOIN likes ON likes.food_id = foods.id
+                            AND likes.user_id = #{user_id}
+    SQL
+    joins(join_sql)
+  end
+
   mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "foods#index"
+  root to: "foods#index"
   resources :foods do
     resource :likes, only: %i[create destroy]
   end
